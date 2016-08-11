4d32589c1e468454dd441fb872054e00d49e0717
Ciaran McManus <ciaranmcmanus@live.ie>
2016-08-11 17:48:53 +0100

    Finished the basic app, lets you add/edit/delete. All items are editable/deleteable. Basic app.

--- dev/shopping-list.component.ts
+++ dev/shopping-list.component.ts
@@ -5,18 +5,45 @@ import {Component} from "angular2/core";
 	//local variable = # before name
 	template: `
 	<ul>
-		<li *ngFor="#shoppingListItem of shoppingListItems">
-			{{ shoppingListItem }}
+		<li *ngFor="#shoppingListItem of shoppingListItems" 
+			(click)="onItemClicked(shoppingListItem)">
+			{{ shoppingListItem.name }}
 		</li>
 	</ul> 
+	<input type="text" [(ngModel)]="selectedItem.name">
+	<button (click)="onDeleteItem(selectedItem)">Delete Item</button><br>
+	<input type="text" #shoppingListItem>
+	<button (click)="onAddItem(shoppingListItem)">Add Item</button>
 	`
 })
 
 export class ShoppingListComponent {
 	
-	public shoppingListItems = ['Milk', 'Sugar', 'Bread'];
+	public shoppingListItems = [
 
-	constructor(argument) {
-		// code...
+	//make objects
+	{name: "Milk"},
+	{name: "Honey"},
+	{name: "Bread"},
+	{name: "Cereal"},
+
+	];
+	public selectedItem = {name: ""};
+
+	onItemClicked(shoppingListItem)
+	{
+		//bind to the sent in item
+		this.selectedItem = shoppingListItem;
 	}
+
+	onAddItem(shoppingListItem){
+		//push out a new object to the shopping list items
+		this.shoppingListItems.push({name: shoppingListItem.value});
+	}
+
+	onDeleteItem(){
+		//push out a new object to the shopping list items
+		this.shoppingListItems.splice(this.shoppingListItems.indexOf(this.selectedItem),1);
+	}
+
 }
